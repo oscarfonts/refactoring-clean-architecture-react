626d0973b5c62126e8d60a5aba20bf943f189dca
Oscar Fonts <oscar.fonts@geomati.co>
2024-07-01 19:04:54 +0200

    Create custom render

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,9 +1,16 @@
+import { ReactNode } from "react";
 import { test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../../App";
+import { render, RenderResult, screen } from "@testing-library/react";
+
+import { AppProvider } from "../../context/AppProvider.tsx";
+import { ProductsPage } from "../ProductsPage.tsx";
 
 test("Loads and displays title", async () => {
-    render(<App />);
+    renderComponent(<ProductsPage />);
 
     await screen.findAllByRole("heading", { name: "Product price updater" });
 });
+
+function renderComponent(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
+}
