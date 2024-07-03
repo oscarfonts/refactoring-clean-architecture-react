973666c760a44cb8adca02f867432bfbae429b3f
Oscar Fonts <oscar.fonts@geomati.co>
2024-07-03 17:29:50 +0200

    Add mws and create MockWebServer wrapper

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,16 +1,39 @@
-import { ReactNode } from "react";
-import { test } from "vitest";
-import { render, RenderResult, screen } from "@testing-library/react";
+import {ReactNode} from 'react';
+import {afterAll, afterEach, beforeAll, describe, test} from 'vitest';
+import {render, RenderResult, screen} from '@testing-library/react';
 
-import { AppProvider } from "../../context/AppProvider.tsx";
-import { ProductsPage } from "../ProductsPage.tsx";
+import {AppProvider} from '../../context/AppProvider.tsx';
+import {ProductsPage} from '../ProductsPage.tsx';
+import {MockWebServer} from '../../tests/MockWebServer.ts';
+import productsResponse from './data/productsResponse.json';
 
-test("Loads and displays title", async () => {
-    renderComponent(<ProductsPage />);
+const mockWebServer = new MockWebServer();
 
-    await screen.findAllByRole("heading", { name: "Product price updater" });
+describe('Products page', () => {
+  beforeAll(() => mockWebServer.start());
+  afterEach(() => mockWebServer.resetHandlers());
+  afterAll(() => mockWebServer.close());
+
+  test('Loads and displays title', async () => {
+    givenAProducts();
+
+    renderComponent(<ProductsPage/>);
+
+    await screen.findAllByRole('heading', {name: 'Product price updater'});
+  });
 });
 
+function givenAProducts() {
+  mockWebServer.addRequestHandlers([
+    {
+      method: 'get',
+      endpoint: `https://fakestoreapi.com/products`,
+      httpStatusCode: 200,
+      response: productsResponse
+    }
+  ]);
+}
+
 function renderComponent(component: ReactNode): RenderResult {
-    return render(<AppProvider>{component}</AppProvider>);
+  return render(<AppProvider>{component}</AppProvider>);
 }
